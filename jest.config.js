556cb7a3e3f2293b1c8698787533d7b63d947605
ViniCosta1 <vinicosta154@hotmail.com>
2025-02-19 14:53:11 -0300

    Express Server Configured

---
+++ jest.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/client/index.js', // Ponto de entrada
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'] // Adiciona suporte para extensões .jsx
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, // Aplica o Babel a arquivos .js e .jsx
+        exclude: /node_modules/,
+        use: 'babel-loader', // Usa o loader do Babel
+      },
+    ],
+  },
+  devtool: 'source-map', // Gera source maps
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    port: 3000,
+    hot: true,
+  },
+};
